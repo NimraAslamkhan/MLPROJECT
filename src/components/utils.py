0a56d681180c12628d578a7ec39d192ca43eeffd
“nimraaslam3132@gmail.com <nimraaslam3132@gmail.com>
2024-03-25 10:30:50 +0500

    model traniner

--- src/components/utils.py
+++ src/components/utils.py
@@ -1,10 +1,14 @@
-import os 
+import os
 import sys
-import numpy as np
+
+import numpy as np 
 import pandas as pd
 import dill
 import pickle
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 from exception import CustomException
+from sklearn.metrics import mean_squared_error
 
 def save_object(file_path, obj):
  try:    
@@ -16,5 +20,31 @@ def save_object(file_path, obj):
         dill.dump(obj, file_obj)
  except Exception as e:
     raise CustomException(e, sys)
-
  
+ 
+
+from sklearn.metrics import mean_squared_error
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    """
+    Evaluate different machine learning models.
+    
+    Parameters:
+        X_train (array-like): Training features.
+        y_train (array-like): Training labels.
+        X_test (array-like): Testing features.
+        y_test (array-like): Testing labels.
+        models (dict): Dictionary containing model names as keys and model objects as values.
+        
+    Returns:
+        dict: Dictionary containing model names as keys and corresponding evaluation scores as values.
+    """
+    model_scores = {}
+    
+    for model_name, model in models.items():
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        score = mean_squared_error(y_test, y_pred)
+        model_scores[model_name] = score
+    
+    return model_scores
